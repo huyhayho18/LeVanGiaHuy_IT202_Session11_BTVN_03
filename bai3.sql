a0b77ce1eeec25d9e3ce902fdff4b2b8a23aa22d
huyhayho18 <giahuylevan1807@gmail.com>
2026-05-08 11:05:19 +0700

    Refactor CalculatePatientFee procedure to set messages

--- bai3.sql
+++ bai3.sql
@@ -1,22 +1,29 @@
+DROP PROCEDURE IF EXISTS CalculatePatientFee;
+
 DELIMITER //
 CREATE PROCEDURE CalculatePatientFee(
     IN  p_patient_type VARCHAR(10),
     IN  p_total_cost DECIMAL(18,2),
-    OUT p_amount_due DECIMAL(18,2)
+    OUT p_amount_due DECIMAL(18,2),
+    OUT p_message VARCHAR(100)
 )
 BEGIN
     IF p_total_cost < 0 THEN
         SET p_amount_due = 0;
-        SELECT 'Lỗi: Chi phí không hợp lệ';
+        SET p_message    = 'Lỗi: Chi phí không hợp lệ';
+
     ELSEIF p_patient_type = 'BHYT' THEN
         SET p_amount_due = p_total_cost * 0.20;
-        SELECT 'Đã tính toán xong';
+        SET p_message    = 'Đã tính toán xong';
+
     ELSEIF p_patient_type = 'VIP' THEN
         SET p_amount_due = p_total_cost * 0.90;
-        SELECT 'Đã tính toán xong';
+        SET p_message    = 'Đã tính toán xong';
+
     ELSEIF p_patient_type = 'THUONG' THEN
         SET p_amount_due = p_total_cost * 1.00;
-        SELECT 'Đã tính toán xong';
+        SET p_message    = 'Đã tính toán xong';
+
     END IF;
 END //
 DELIMITER ;
